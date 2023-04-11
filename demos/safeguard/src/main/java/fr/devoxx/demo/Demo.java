d8491eb2b0744c70f7352259471370701a8b10f3
fteychene <francois.teychene@gmail.com>
2023-04-11 17:37:26 +0200

    update prez

--- demos/safeguard/src/main/java/fr/devoxx/demo/Demo.java
+++ demos/safeguard/src/main/java/fr/devoxx/demo/Demo.java
@@ -2,9 +2,6 @@ package fr.devoxx.demo;
 
 import io.conduktor.gateway.Bootstrap;
 
-import java.lang.reflect.Field;
-import java.util.Map;
-
 public class Demo {
 
     public static void main(String[] args) {
